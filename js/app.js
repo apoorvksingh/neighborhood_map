9a4e6cf75d964b4e088e800ade846436b346dbe5
Apoorv Kumar Singh <apoorvksingh@hotmail.com>
2016-07-19 20:18:05 +0530

    Updated search filter capabitlity

--- js/app.js
+++ js/app.js
@@ -6,15 +6,30 @@ var locations = [
                       {title: 'The Beer Cafe', location: {lat: 28.495522, lng: 77.089080}},
                       {title: 'Soi 7', location: {lat: 28.495843, lng: 77.088562}}
                 ];
-
 var Location = function(data) {
     this.title = ko.observable(data.title);
 };
+var filter = function() {
+  var self = this;
+  this.locList.removeAll();
+  console.log(this.locList());
+  var keyword = document.getElementById("search-bar").value;
+  console.log(keyword);
+  locations.forEach(function(locationItem){
+    if (locationItem.title.toUpperCase().includes(keyword.toUpperCase())) {
+     self.locList.push(new Location(locationItem));
+    }
+    console.log(locations);
+  })
+
+}
 var ViewModel = function() {
     var self = this;
     this.locList = ko.observableArray([]);
     locations.forEach(function(locationItem){
         self.locList.push(new Location(locationItem));
     })
+
 }
 ko.applyBindings(new ViewModel());
+//cc78361027869dd4bd7d23945a095e97
